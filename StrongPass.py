8d2d86c913672421c8030374206ba448b75220ac
DarkZero <ruanvsd2024@gmail.com>
2025-10-08 21:49:55 -0300

    Update StrongPass.py

--- StrongPass.py
+++ StrongPass.py
@@ -3,18 +3,21 @@
 Author: DarkZer0
 Name: StrongPass
 Function: Strong Password Generator
-Version: 1.0
+Version: 1.5
 
 """
 
 # importa as bibliotecas
 import string
 import random
-from colorama import init, Fore
+import time
 
 import os
 import sys
 import pyfiglet
+
+from colorama import init, Fore
+from datetime import datetime
 from threading import Thread
 
 # inicia o colorama
@@ -39,6 +42,8 @@ def gerar_senha():
     try:
         gerar = string.ascii_letters + string.digits + string.punctuation
         
+        tempo_inicial = time.time()
+        
         while True:
             tamanho = input(Fore.CYAN+"tamanho da senha: ")
             
@@ -51,10 +56,44 @@ def gerar_senha():
                 
                 gerando = ''.join(random.choice(gerar) for _ in range(inteiro))
                 print(Fore.BLUE+f"\nsenha gerada: {gerando}\n")
+                
+                salvar = str(input(Fore.YELLOW+"\nDeseja salvar a senha em .txt? (y ou n): "))
+                
+                if salvar == "y":
+                    with open("/sdcard/dcim/senha.txt", "w") as f:
+                        f.write(gerando)
+                        print(Fore.GREEN+"\nsenha salva com sucesso em /sdcar/dcim/senha.txt\n")
+                        
+                        print(Fore.CYAN+"data e hora: ", datetime.now())
+                        tempo_final = time.time()
+                        
+                        total_tempo = int(tempo_final - tempo_inicial)
+                        
+                        h, remainder = divmod(total_tempo, 3600)
+                        m, s = divmod(remainder, 60)
+                        
+                        print(Fore.CYAN+f"tempo de execução: {h}h {m}m {s}s\n")
+                        exit()
+                    
+                if salvar == "n":
+                    print(Fore.RED+"\nbeleza, senha não salva.\n")
+                    
+                    print(Fore.CYAN+"data e hora: ", datetime.now())
+                    
+                    tempo_final = time.time()
+                    
+                    total_tempo = int(tempo_final - tempo_inicial)
+                    h, remainder = divmod(total_tempo, 3600)
+                    
+                    m, s = divmod(remainder, 60)
+                    print(Fore.CYAN+f"tempo de execução: {h}h {m}m {s}s\n")
+                    
+                    exit()
         
     except Exception as e:
         print(Fore.RED+f"\nerro: {e}\n")
-        
+        quit()
+    
 if __name__=="__main__":
     Thread(target=gerar_senha).start()
 
